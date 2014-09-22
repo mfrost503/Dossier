79d85d9624ea69ada146df6d9b2220e48724b234
Matt Frost <mfrost.design@gmail.com>
2014-09-22 12:21:53 -0400

    Adding some bootstrap stuff, creating the beginning of a registration form (with controller) and other small sorts of tom-foolery

--- public/scripts/config.js
+++ public/scripts/config.js
@@ -1,3 +1,5 @@
 dossierApp.config(['$routeProvider', '$locationProvider', '$httpProvider', '$resourceProvider', function($routeProvider, $locationProvider, $httpProvier, $resouceProvider) {
+	$locationProvider.html5Mode(true);
 	$routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'HomeController'});
+	$routeProvider.when('/register', {templateUrl: 'partials/register.html', controller: 'RegistrationController'});
 }]);
---
+++ public/scripts/controllers/RegistrationController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+dossierApp.controller('RegistrationController', ['$scope', '$rootScope', function($scope, $rootScope) {
+	$scope.registration = {firstName: '', lastName: '', email: '', password: '', confirmPassword: ''};
+
+	$scope.register = function() {
+		// process the values from the form here and pass them to the API/Service to register the user
+		// successful registration should show confirmation page, failed registration should show failed
+		// registration page...
+	};
+}]);
