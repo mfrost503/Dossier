9dc407d17d61b285a353e291dda4b6ce04be18c5
Matt Frost <mfrost.design@gmail.com>
2014-09-24 00:18:23 -0400

    Adding some more stuff in before I have to tear this box apart and make a new one...because I have no idea what Im doing apparently

--- public/scripts/controllers/LoginController.js
+++ public/scripts/controllers/LoginController.js
@@ -1,10 +1,39 @@
 'use strict';
 
-dossierApp.controller('LoginController', ['$scope', '$rootScope', function($scope, $rootScope) {
-	$scope.credentials = {username: '', password: ''};
-	
-	$scope.login = function() {
-		// pass username and password to a secure endpoint in order to be processed
-		// display an error if the login fails, direct them home on successful login
-	};
+dossierApp.controller('LoginController', ['$scope', '$rootScope', '$location', '$http', function($scope, $rootScope, $location,$http) {
+	var queryString = $location.search();
+	var clientId = '825e4ea00c36c458ff3a';
+	if (typeof queryString.code === 'undefined') {
+		var callback = 'http://dossier.dev/login';
+		var state = Math.random();
+		var url = "https://github.com/login/oauth/authorize?client_id="+clientId+"&redirect_uri="+callback+"&state="+state;
+		window.location = url;
+	} else {
+		var code = queryString.code;
+		var clientSecret = '29d73b6267e5fb4d85642d8fd1a97f425f19085e';
+		var redirect = 'http://dossier.dev/';
+		$http({
+			method:'POST',
+			url: 'proxy/githubToken.php',
+			data: {
+				client_id: clientId,
+				client_secret: clientSecret,
+				code: code,
+				redirect_uri: redirect
+			},
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Expose-Headers': 'ETag, Link, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Reset, X-OAuth-Scopes, X-Accepted-OAuth-Scopes',
+				'Access-Control-Allow-Headers': 'Authorization, Content-Type, If-Match, If-Modified-Since, If-None-Match, If-Unmodified-Since, X-Requested-With',
+				'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE',
+				'Access-Control-Max-Age': 86400
+			}
+		})
+		.success(function(data, status, headers, config) {
+			alert('success ' +data);
+		})
+		.error(function(data, status, headers, config) {
+			alert('error ' + status);
+		});
+	}		
 }]);
